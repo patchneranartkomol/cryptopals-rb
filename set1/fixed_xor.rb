387312e0fbc653d0cda144f15145117176ad9df2
Patch Neranartkomol <patch@neranartkomol.com>
2022-11-04 13:45:21 -0700

    S1C2 - Fixed XOR

---
+++ set1/fixed_xor.rb
@@ -0,0 +1,6 @@
+def fixed_xor(ahex, bhex)
+  a = [ahex].pack("H*").bytes
+  b = [bhex].pack("H*").bytes
+  out = a.zip(b).map{|x, y| x ^ y}
+  out.pack("c*").unpack1("H*")
+end
---
+++ set1/fixed_xor_test.rb
@@ -0,0 +1,8 @@
+require_relative "fixed_xor"
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def test_fixed_xor
+    assert_equal(fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"), "746865206b696420646f6e277420706c6179")
+  end
+end
