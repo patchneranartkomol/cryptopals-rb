e36b8329472c4a1e2806156cbbd94942a420b193
Patch Neranartkomol <patch@neranartkomol.com>
2022-11-05 22:43:03 -0700

    Set 1 Challenge 3 - Single-byte XOR cipher in Ruby

---
+++ set1/single_byte_xor_cipher.rb
@@ -0,0 +1,27 @@
+$max_score = 0
+$result = ""
+
+def determine_message(s)
+  b = [s].pack("H*").bytes
+  ('A'..'Z').each { |ch| check_xor(ch, b) }
+  ('a'..'z').each { |ch| check_xor(ch, b) }
+end
+
+def check_xor(ch, b)
+  decoded = b.map { |x| (x ^ ch.ord).chr }
+  count = 0
+  decoded.each do |x|  
+    if "ETAOIN SHRDLUetaoinshrdlu".include? x
+      count += 1
+    end
+  end
+  if count > $max_score
+    $max_score = count
+    $res = decoded.join('')
+  end
+end
+
+if __FILE__ == $0
+  determine_message("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  puts $res
+end
