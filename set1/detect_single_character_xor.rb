d406890c6132b48292ff1a5c3291fac08e271d8a
Patch Neranartkomol <patch@neranartkomol.com>
2022-11-06 19:34:13 -0800

    Set 1 Challenge 4 - Now that the party is jumping

---
+++ set1/detect_single_character_xor.rb
@@ -0,0 +1,29 @@
+$max_score = 0
+$result = ""
+
+def check_message(s)
+  b = [s].pack("H*").bytes
+  ('0'..'z').each { |ch| check_xor(ch, b) }
+end
+
+def check_xor(ch, b)
+  decoded = b.map { |x| (x ^ ch.ord).chr }
+  count = 0
+  decoded.each do |x|  
+    if "ETAOIN SHRDLUetaoinshrdlu".include? x
+      count += 1
+    end
+  end
+  if count > $max_score
+    $max_score = count
+    $res = decoded.join('')
+  end
+  decoded_str = decoded.join('')
+end
+
+if __FILE__ == $0
+  File.readlines('4.txt').each do |line|
+    check_message(line)
+  end
+  puts $res
+end
